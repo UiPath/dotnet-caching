32d4fab0aa908df26bc33bf40367eec26e58388d
cosmin-staicu <cosmin.staicu@uipath.com>
2023-12-15 16:15:57 +0200

    Caching: Cleanup generic class constrain

--- Caching.Runtime/CacheFactoryExtensions.cs
+++ Caching.Runtime/CacheFactoryExtensions.cs
@@ -2,9 +2,9 @@
 
 public static class CacheFactoryExtensions
 {
-    public static ICache<T> CreateCache<T>(this ICacheFactory factory, string? providerName = null, Type? callerType = null) where T : class =>
+    public static ICache<T> CreateCache<T>(this ICacheFactory factory, string? providerName = null, Type? callerType = null) =>
         new Cache<T>(factory.CreateCache(providerName, typeof(T), callerType));
 
-    public static IHashCache<T> CreateHashCache<T>(this ICacheFactory factory, string? providerName = null, Type? callerType = null) where T : class =>
+    public static IHashCache<T> CreateHashCache<T>(this ICacheFactory factory, string? providerName = null, Type? callerType = null) =>
         new HashCache<T>(factory.CreateHashCache(providerName, typeof(T), callerType));
 }
